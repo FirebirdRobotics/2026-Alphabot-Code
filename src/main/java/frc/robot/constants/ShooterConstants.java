fabd04aad8dd9303444688e0695e903bae997850
Manish <ysmanish5@gmail.com>
2026-01-11 23:45:24 -0700

    added shooter constants

---
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -0,0 +1,14 @@
+package frc.robot.constants;
+
+public class ShooterConstants {
+    public static final int elevatorLeaderMotorID = 46;
+    public static final int elevatorFollowerMotorID = 48;
+    public static final int elevatorCANcoderID = 60;
+
+
+
+
+
+
+
+}
