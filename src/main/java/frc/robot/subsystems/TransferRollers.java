dc27c7b0303ee4c0eccc819cacb2259b4d620b81
flaeime <tpadgett123@gmail.com>
2026-01-15 15:42:51 -0700

    transfer rollers setup, added basic roll forward and backward commands

---
+++ src/main/java/frc/robot/subsystems/TransferRollers.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.hardware.CANrange;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class TransferRollers extends SubsystemBase {
+  /** Creates a new TransferRollers. */
+  private final TalonFX m_rollerMotor = new TalonFX(42, "CANivore"); //change device ID
+  private final CANrange intakeCANrange = new CANrange(45, "CANivore"); //change device ID
+  
+  public TransferRollers() {}
+
+  public void setRollerMotorPercentOutput(double outputPercent) {
+    m_rollerMotor.setControl(new DutyCycleOut(outputPercent));
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public Command manualRollForwards(double power) {
+    return run(
+      () -> setRollerMotorPercentOutput(power)
+    );
+  }
+
+  public Command manualRollBackward(double power) {
+    return run(
+      () -> setRollerMotorPercentOutput(power * -1)
+    );
+  }
+}
